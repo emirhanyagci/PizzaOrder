740c6c1a985881f728faf3efa4a4d7e5049b568a
Emirhan Yağcı <emirhan.yac2@gmail.com>
2023-08-09 19:06:06 +0300

    card option buttons moved into card

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -20,55 +20,58 @@ function CreditCard({
         onMouseLeave={() => (editable ? setShowOption(false) : null)}
       >
         <div className=" bg-gradient-to-t h-36 from-cardGray-dark to-cardGray-light w-full  p-5 shadow-slate-900 shadow-md flex flex-col justify-between">
-          <div className="flex justify-between">
-            <div className="flex flex-col">
-              <span className="text-lightGray">Current Balance :</span>
-              <span className="text-2xl font-medium">
-                {parseInt(currentBalance, 10)}$
-              </span>
+          {showOption ? (
+            <div className="w-full h-full flex items-center py-3">
+              <Button
+                onClickHandler={() => {
+                  selectSelectedCard({
+                    cartId,
+                    currentBalance,
+                    cartNumber,
+                    lastDate,
+                  });
+                }}
+                className="w-1/2 bg-secondary-500 rounded-tl-md rounded-bl-md py-1"
+                disabled={wallets[0].cartId === cartId}
+              >
+                Select
+              </Button>
+              <Button
+                onClickHandler={() => {
+                  removeFromCards({
+                    cartId,
+                    currentBalance,
+                    cartNumber,
+                    lastDate,
+                  });
+                }}
+                className="w-1/2 py-1"
+              >
+                Remove
+              </Button>
             </div>
-            <div>
-              <img src="/public/mc_symbol.svg" width="56px" alt="" />
-            </div>
-          </div>
-          <div className="flex justify-between">
-            <span className="">{cartNumber}</span>
-            <span>
-              {parseInt(lastDate.month, 10)}/{lastDate.year}
-            </span>
-          </div>
+          ) : (
+            <>
+              <div className="flex justify-between">
+                <div className="flex flex-col">
+                  <span className="text-lightGray">Current Balance :</span>
+                  <span className="text-2xl font-medium">
+                    {parseInt(currentBalance, 10)}$
+                  </span>
+                </div>
+                <div>
+                  <img src="/public/mc_symbol.svg" width="56px" alt="" />
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <span className="">{cartNumber}</span>
+                <span>
+                  {parseInt(lastDate.month, 10)}/{lastDate.year}
+                </span>
+              </div>
+            </>
+          )}
         </div>
-        {showOption ? (
-          <div className="w-full py-3">
-            <Button
-              onClickHandler={() => {
-                selectSelectedCard({
-                  cartId,
-                  currentBalance,
-                  cartNumber,
-                  lastDate,
-                });
-              }}
-              className="w-1/2 bg-secondary-500 rounded-tl-md rounded-bl-md py-1"
-              disabled={wallets[0].cartId === cartId}
-            >
-              Select
-            </Button>
-            <Button
-              onClickHandler={() => {
-                removeFromCards({
-                  cartId,
-                  currentBalance,
-                  cartNumber,
-                  lastDate,
-                });
-              }}
-              className="w-1/2 bg-cardGray-light rounded-tr-md rounded-br-md py-1"
-            >
-              Remove
-            </Button>
-          </div>
-        ) : null}
       </div>
     </>
   );
