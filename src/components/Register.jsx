fafbd9d5718c7a2ef7a130a6602002f862fc7954
Emirhan Yağcı <emirhan.yac2@gmail.com>
2023-08-10 20:16:03 +0300

    auth page b-radius bug fixed

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -18,8 +18,8 @@ function Register() {
   return (
     <section className="bg-gray-50 ">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0  ">
-          <div className="p-6 bg-gray-800/10 space-y-4 md:space-y-6 sm:p-8">
+        <div className="w-full rounded-lg bg-gray-800/10 shadow md:mt-0 sm:max-w-md xl:p-0  ">
+          <div className="p-6  space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl mb-3">
               Create and account
             </h1>
